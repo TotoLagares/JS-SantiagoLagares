027fa4c8b21428ab75a0276b7fc9380a48ca6fdf
Santiago Lagares <santiagolagares2005@gmail.com>
2023-01-31 16:35:31 -0300

    Pre_entega2

--- js/main.js
+++ js/main.js
@@ -1,84 +1,44 @@
 
 //filtro (modelos,Marcas)
-function Filtro(){
-   let marca = prompt ("¿Que zapatillas busca? Adidas - Nike - Jordan" )
-   console.log("Marca ingresada;" , marca );
-   if (((marca !== "adidas") && (marca !== "jordan") && (marca !== "nike"))){
-     alert("Marca NO disponible")
+   const producto = [
+      {modelo: "yeezy 700", valor: 300},
+      {modelo: "yeezy 350", valor:240},
+      {modelo: "human race", valor:200},
+      {modelo: "air max 1", valor:73},
+      {modelo: "air force", valor:120},
+      {modelo: "huarache", valor:100},
+      {modelo: "jordan 1 white", valor:125},
+      {modelo: "jordan 4 blue", valor:250},
+      {modelo: "uptempo", valor:88},
+   ]
+   function int(){
+      let producto_final = prompt ("¿Que modelo desea?")
+      //seguir
+      let seguir =prompt("desea comprar algo mas?");
+      if(seguir == "si"){
+        int() 
    }   
-        if (marca == "adidas") {
-        modelo = prompt ("¿Que Modelo?  Yeezy700 (300 usd) - Yeezy350 (240 usd) - HumanRace (200 Usd)");
-           
-        }
-        if( marca == "jordan"){ 
-         modelo = prompt ("¿Que Modelo?  Jordan 1 white- Jordan 4 blue - uptempo");
-        }
-        if ( marca == "nike"){ 
-         modelo = prompt ("¿Que Modelo?  Air Max 1 - Air Force - Huarache") 
-        }
-        console.log("modelo Elegido;",modelo,);
+   const productoselecto = producto.find((producto) => producto.modelo === producto_final)
+   console.log(productoselecto);
 }
-Filtro()
-//seguir 
-let seguir = prompt("desea comprar algo mas?");
-        if(seguir == "si"){
-           Filtro()
-      }
-     let mosFiltro= Filtro()
+int()
+   let cuotas = parseInt (prompt("En cuantas cuotas con interes desea pagar 3 - 6 - 12"))
+   const interes = [
+            {cuota: 3, interesAgregado:producto.valor*0.15},
+            {cuota: 6, interesAgregado: producto.valor*0.35},
+            {cuota: 12, interesAgregado: producto.valor*0.55},]
+   const interesfinal = interes.find((interes) => interes.cuota == cuotas)
+      
+   console.log("Valor mas Interes;",productoselecto + interesfinal.interesAgregado  )
+
+
+   
+
+    
 //interes x cuotas
-     let cuotas = parseInt (prompt("En cuantas cuotas con interes desea pagar 3 - 6 - 12")) 
-      function interes_cuotas (valor, cuotas){
-      let interes;
-  
-      if (cuotas == 3){
-         interes = valor * 0.15;
-         return interes
-      }
-      if (cuotas == 6){
-         interes = valor * 0.40;
-         return interes
-      }
-      if (cuotas == 12){
-         interes = valor * 0.90;
-         return interes
-      }else{
-         return false
-      }  
-   }
+     
+       
+      
 
-//valor modelos
-const precio = [
-   {modelo: "yeezy700", valor: 300},
-   {modelo: "yeezy350", valor:240},
-   {},
 
-]
-const valor = precio.map((el)=> el.valor)	
-/* 
-   case "humanrace":
-      valor = 200;
-      break;
-   case "air max 1":
-      valor = 73;
-      break;
-   case "air force":
-      valor = 120;
-      break;
-   case "huarache":
-      valor = 100;
-      break;
-   case "jordan 1 white":
-      valor = 125;
-      break;
-   case "jordan 4 blue":
-      valor = 250;
-      break;
-   case "uptempo":
-      valor = 88;
-      break;   
-	default:
-		valor = 0;
-		break;
-}*/
-console.log("Valor;",valor)
-console.log("Valor mas Interes;",valor + interes_cuotas (valor, cuotas))
+
