e6516a9e71f49eec28fa7e15915150e1e4420cb2
Santiago Lagares <santiagolagares2005@gmail.com>
2023-01-31 18:17:28 -0300

    Segunda Entrega

--- js/main.js
+++ js/main.js
@@ -11,25 +11,33 @@
       {modelo: "jordan 4 blue", valor:250},
       {modelo: "uptempo", valor:88},
    ]
+   
    function int(){
       let producto_final = prompt ("¿Que modelo desea?")
       //seguir
-      let seguir =prompt("desea comprar algo mas?");
-      if(seguir == "si"){
-        int() 
-   }   
-   const productoselecto = producto.find((producto) => producto.modelo === producto_final)
+      
+   productoselecto = producto.find((producto) => producto.modelo === producto_final)
    console.log(productoselecto);
-}
-int()
+   let sumarIva = productoselecto.valor * 1.21
+   console.log ("Valor + Iva",sumarIva);
+
+   
+   //prueba
    let cuotas = parseInt (prompt("En cuantas cuotas con interes desea pagar 3 - 6 - 12"))
    const interes = [
-            {cuota: 3, interesAgregado:producto.valor*0.15},
-            {cuota: 6, interesAgregado: producto.valor*0.35},
-            {cuota: 12, interesAgregado: producto.valor*0.55},]
-   const interesfinal = interes.find((interes) => interes.cuota == cuotas)
-      
-   console.log("Valor mas Interes;",productoselecto + interesfinal.interesAgregado  )
+      {cuota: 3, intereses: 1.15 },
+      {cuota: 6, intereses: 1.35 },
+      {cuota: 12, intereses:  1.55 },
+   ]
+   let ValorFinal = (interes.find((interes) => interes.cuota == cuotas))
+   console.log ("Valor Final (Iva + Interes)",ValorFinal.intereses * sumarIva)
+   let seguir =prompt("desea comprar algo mas?");
+      if(seguir == "si"){
+        int() 
+   }
+}
+int()
+
 
 
    
@@ -38,7 +46,4 @@ int()
 //interes x cuotas
      
        
-      
-
-
-
+   
